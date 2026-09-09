4e4c2a0e5600c373858145dcfd16d6c8b6cf3f13
Kmundongofrito <gianemolo@gmail.com>
2026-09-08 22:20:16 -0300

    .
    .

---
+++ src/database/migrations/20260909011028_create_filmes_generos.js
@@ -0,0 +1,31 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.up = function(knex) {
+     return knex.schema.createTable("locacoes", (table) => {
+         table.increments("id").primary()
+
+        table.integer("filme_id")
+        .unsigned()
+        .notNullable()
+        .references("id")
+        .inTable("filmes")
+        .onDelete("CASCADE")
+
+        table.integer("genero_id")
+        .unsigned()
+        .notNullable()
+        .references("id")
+        .inTable("generos")
+        .onDelete("CASCADE")
+     })
+};
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.down = function(knex) {
+  
+};
